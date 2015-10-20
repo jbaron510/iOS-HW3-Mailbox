7a997d1e93d9fcef4b3519995e684308113e7b20
jbaron510 <jbaron@walmart.com>
2015-10-20 14:48:38 -0700

    Initial Commit

---
+++ iOS-HW3-Mailbox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS-HW3-Mailbox
+//
+//  Created by Jon Baron on 10/20/15.
+//  Copyright © 2015 Walmart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
